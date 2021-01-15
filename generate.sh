68ba8b7556df5bd23d3724229b8b688c4b5168b8
williambl <william@williambl.com>
2021-01-15 12:49:59 +0000

    Add build command to generate script

--- generate.sh
+++ generate.sh
@@ -1,1 +1,2 @@
 swig -java -package com.valvesoftware.phonon -outdir generated phonon.i
+gcc -g3 -fPIC -Wall -Wextra -shared phonon_wrap.c -o libphononjni.so -L. -lphonon -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux
